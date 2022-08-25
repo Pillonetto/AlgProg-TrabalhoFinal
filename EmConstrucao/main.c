46efcc48579ef49da38db33bb7c7d50e83d7509d
Gabriel Pillonetto <gabrielpillonetto@gmail.com>
2022-08-24 21:42:32 -0300

    Funcao de preenchimento do vetor caixas

--- EmConstrucao/main.c
+++ EmConstrucao/main.c
@@ -1,10 +1,22 @@
 #include "raylib.h"
-#include "controleMatriz.h"
+
+//Adicionar às constantes do jogo
+#define BOMBA 1
+#define CHAVE 2
+#define MAX_CAIXAS 7
+
+//Probabilidade de cada item (numero aleatorio gerado vai de 0 a 9)
+#define COROA (item == 9) //10% 
+#define ANEL (item == 8) //10%
+#define RUBI (item > 5) //(6, 7) -> 20%
+#define SAFIRA (item > 2) //(3, 4, 5) -> 30%
+                        //Ametista (0, 1, 2) -> 30%
 
 
 /* Funcao recebe o número de caixas da fase e 
     gera array com o valor dos bens aleatórios + chave 
-    Bombas = 1, Chave = 2, Bens = pontuação respectiva */
+    Bombas = 1, Chave = 2, Bens = pontuação respectiva 
+    OBS: Inicializar vetor de caixas com 0s   */
 
 void geraItens (int numeroCaixas, int fase, int caixas[MAX_CAIXAS]){
 
@@ -27,7 +39,35 @@ void geraItens (int numeroCaixas, int fase, int caixas[MAX_CAIXAS]){
     }
 
     //Para caixas restantes, gerar bens aleatórios
-    
+
+    for (int j = 0; j < numeroCaixas - fase){
+
+        //Gera indice aleatorio entre 0 e o número de caixas - 1
+        index = rand() % numeroCaixas;
+
+        //Se caixa estiver vazia
+        if (caixas[index] == 0){
+
+            //Gera numero aleatorio entre 0 e 9 para definir item
+            int item = rand() % 10;
+
+            //Interpretacao do numero aleatorio gerado
+            if (COROA)
+                caixas[index] = 300;
+            else if (ANEL)
+                caixas[index] = 200;
+            else if (RUBI)
+                caixas[index] = 150;
+            else if (SAFIRA)
+                caixas[index] = 100;
+            else
+                caixas[index] = 50;
+
+        }
+
+
+    }
+
 
 }
 
