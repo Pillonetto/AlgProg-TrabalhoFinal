90b49c8eb43ab11c7707439ffca2790e2129ccbf
Gabriel Pillonetto <gabrielpillonetto@gmail.com>
2022-08-24 21:42:57 -0300

    Constantes de caixa alteradas para arquivo main.c

--- controleMatriz.h
+++ controleMatriz.h
@@ -6,9 +6,6 @@
 
 #define MAPA_L 30
 #define MAPA_C 120
-#define MAX_CAIXAS 7
-#define BOMBA 1
-#define CHAVE 2
 
 void carrega_mapa(char mapa[MAPA_L][MAPA_C], int *linhas, int *colunas, char *porta, int *escada);
 void imprime_mapa(char mapa[MAPA_L][MAPA_C], int linhas, int colunas);
